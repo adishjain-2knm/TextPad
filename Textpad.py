dea72b6a55b3b3892b75e03a1738a006622d7a4b
adishjain-2knm <54764581+adishjain-2knm@users.noreply.github.com>
2021-11-28 21:45:41 +0530

    Notebook test
    Testing to implement menu bar using Notebook widget,
    have some placement and resizing issues

--- Textpad.py
+++ Textpad.py
@@ -1,14 +1,26 @@
 import tkinter as tk
 from tkinter.messagebox import showinfo
+from tkinter import ttk
 
 class TextPad:
  
     __root = tk.Tk()
- 
+    __root.config(background="red")
+    
     # default window width and height
     __thisWidth = 300
     __thisHeight = 300
     __TextArea = tk.Text(__root)
+    __notebool_tabs = ttk.Notebook(__root)
+    __notebool_tabs.grid(row=0,sticky="EW")
+    __option1 = tk.Label(__root,text="option1")
+    __option1.grid(row=1,sticky="E")
+    __option2 = tk.Label(__root,text="option2")
+    __option2.grid(row=1,sticky="W")
+    __option3 = tk.Label(__root,text="option3")
+    __option3.grid(row=1,sticky="E")
+    __option4 = tk.Label(__root,text="option4")
+    __option4.grid(row=1,sticky="E")
 
     def __todo():
         showinfo("warning temp","To be implemented")
@@ -58,7 +70,12 @@ class TextPad:
         self.__root.grid_columnconfigure(0, weight=1)
  
         # Add controls (widget)
-        self.__button.grid()
+        #self.__button.grid()
+        self.__notebool_tabs.add(self.__option1,text="menu 1")
+        self.__notebool_tabs.add(self.__option2,text="menu 2")
+        self.__notebool_tabs.add(self.__option3,text="menu 3")
+        self.__notebool_tabs.add(self.__option4,text="menu 4")
+
         self.__TextArea.grid(sticky = tk.N + tk.E + tk.S +tk.W)            
  
     def run(self):
